82a58b498388889d0aad91c288931ef94a541068
Nikolay Savenko <savenkons@yandex.ru>
2022-06-20 23:50:39 +0300

    feat: add initial functions project

---
+++ TeamsMigrationFunction/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+using TeamsMigrationFunction;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace TeamsMigrationFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            // TODO change target tenant creds
+            builder.Services.AddTenantGraphClient(
+                Environment.GetEnvironmentVariable("SourceTenantId") ?? throw new ArgumentNullException("SourceTenantId","No SourceTenantId provided"),
+                Environment.GetEnvironmentVariable("SourceClientId") ?? throw new ArgumentNullException("SourceClientId","No SourceClientId provided"),
+                Environment.GetEnvironmentVariable("SourceClientSecret") ?? throw new ArgumentNullException("SourceClientSecret", "No SourceClientSecret provided")
+            );
+        }
+    }
+}
---
+++ TeamsMigrationFunction/configureApplicationAccessPolicy.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module MicrosoftTeams
+$userCredential = Get-Credential
+Connect-MicrosoftTeams -Credential $userCredential
+
+New-CsApplicationAccessPolicy -Identity MeetingAccessPolicy -AppIds "08be4766-f2bf-4af6-9157-19a7257483e7" -Description "Daemon can work with this user ids"
+
+Get-CsOnlineUser | Grant-CsApplicationAccessPolicy -PolicyName MeetingAccessPolicy
+
+#Remove-CsApplicationAccessPolicy -Identity MeetingAccessPolicy

c1c0ee26ab3497a3992472cc3eea138fe6cb990d
Nikolay Savenko <savenkons@yandex.ru>
2022-06-21 00:00:15 +0300

    feat: add events migration

---
+++ TeamsMigrationFunction/EventMigration/EventMigrationOrchestration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Services;
+using TeamsMigrationFunction.UserMapping;
+
+namespace TeamsMigrationFunction.EventMigration
+{
+    public class EventMigrationOrchestration
+    {
+        private readonly TenantGraphClient _tenantClient;
+
+        public EventMigrationOrchestration(TenantGraphClient tenantClient)
+        {
+            _tenantClient = tenantClient;
+        }
+
+        [FunctionName(nameof(RunEventMigrationOrchestration))]
+        public async Task<Event> RunEventMigrationOrchestration(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            var sourceEvent = context.GetInput<Event>();
+            if (!context.IsReplaying) log.LogInformation("[Migration] Migrating event with subject: {SourceEventSubject}", sourceEvent.Subject);
+            var mapperEntityId = new EntityId(nameof(UserMapper), "global");
+            var mapperProxy = context.CreateEntityProxy<IUserMapper>(mapperEntityId);
+            
+            sourceEvent.Organizer.EmailAddress.Address = await mapperProxy.GetUserDestinationUpn(sourceEvent.Organizer.EmailAddress.Address);
+            sourceEvent.Attendees = await Task.WhenAll(
+                sourceEvent.Attendees
+                    .Select(attendee => mapperProxy.GetDestinationAttendee(attendee))
+            );
+            
+            var recreatedEvent = await context.CallActivityAsync<Event>(nameof(RecreateOnlineMeetingEvent), sourceEvent);
+            await context.CallActivityAsync(nameof(CancelDeprecatedEvent), sourceEvent);
+            return recreatedEvent;
+        }
+
+        [FunctionName(nameof(RecreateOnlineMeetingEvent))]
+        public async Task<Event> RecreateOnlineMeetingEvent([ActivityTrigger] Event sourceEvent)
+        {
+            return await _tenantClient.RecreateOnlineMeetingEvent(sourceEvent);
+        }
+        
+        [FunctionName(nameof(CancelDeprecatedEvent))]
+        public async Task CancelDeprecatedEvent([ActivityTrigger] Event @event, ILogger log)
+        {
+            try
+            {
+                await _tenantClient.CancelDeprecatedEvent(@event);
+            }
+            catch (ServiceException e)
+            {
+                log.LogError("[Migration] Failed to cancel meeting with subject {EventSubject} for user {UserName} with id {EventId}", @event.Subject, @event.Organizer.EmailAddress.Address, @event.Id);
+            }
+        }
+    }
+}
---
+++ TeamsMigrationFunction/EventMigration/EventsOrchestration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Services;
+using TeamsMigrationFunction.UserConfiguration;
+
+namespace TeamsMigrationFunction.EventMigration
+{
+    public class EventsOrchestration
+    {
+        private readonly TenantGraphClient _tenantClient;
+
+        public EventsOrchestration(TenantGraphClient tenantClient)
+        {
+            _tenantClient = tenantClient;
+        }
+        
+        [FunctionName(nameof(RunEventsOrchestration))]
+        public async Task<IEnumerable<Event>> RunEventsOrchestration(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            var user = context.GetInput<User>();
+            
+            await ConfigureUserMailboxStartTime(context, user);
+
+            var organizedEvents = await context.CallActivityAsync<Event[]>(nameof(GetMeetingEventsOrganizedByUser), user);
+            if (!context.IsReplaying) log.LogInformation("[Migration] Found {OrganizedEventsLength} events for user: {UserUserPrincipalName}. Starting migration", organizedEvents.Length, user.UserPrincipalName);
+            return await Task.WhenAll(
+                organizedEvents.Select(
+                    @event => context.CallSubOrchestratorAsync<Event>(nameof(EventMigrationOrchestration.RunEventMigrationOrchestration), @event)
+                )
+            );
+        }
+
+        private static async Task ConfigureUserMailboxStartTime(IDurableOrchestrationContext context, User user)
+        {
+            var entityId = new EntityId(nameof(UserConfiguration.UserConfiguration), user.UserPrincipalName);
+            var configProxy = context.CreateEntityProxy<IUserConfiguration>(entityId);
+            var mailboxStartTime = await configProxy.GetMailboxStartTime();
+            user.AdditionalData ??= new Dictionary<string, object>();
+            user.AdditionalData.Add(UserConfiguration.UserConfiguration.UserAdditionalFieldName, mailboxStartTime);
+        }
+
+        [FunctionName(nameof(GetMeetingEventsOrganizedByUser))]
+        public async Task<IEnumerable<Event>> GetMeetingEventsOrganizedByUser([ActivityTrigger] User user, ILogger log)
+        {
+            try
+            {
+                var dateTimeFrom = user.AdditionalData[UserConfiguration.UserConfiguration.UserAdditionalFieldName] as string;
+                log.LogInformation("[Migration] Scanning meeting for {UserName} from {StartTime}", user.UserPrincipalName, dateTimeFrom);
+                return await _tenantClient.GetMeetingEventsByUser(user, dateTimeFrom);
+            }
+            catch (ServiceException e)
+            {
+                log.LogError(e, "[Migration] Cannot get events for user {UserUserPrincipalName}", user.UserPrincipalName);
+            }
+            return Array.Empty<Event>();
+        }
+    }
+}

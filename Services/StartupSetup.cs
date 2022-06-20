5fc1c150bc84fe8c78cfbfb94886a93847b9c287
Nikolay Savenko <savenkons@yandex.ru>
2022-06-20 23:49:17 +0300

    feat: add graph service helper

---
+++ Services/StartupSetup.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+
+
+namespace Services
+{
+    public static class StartupSetup
+    {
+        private const string ApiUrl = "https://graph.microsoft.com/";
+
+        private const string Instance = "https://login.microsoftonline.com/{0}";
+
+        public static void AddTenantGraphClient(this IServiceCollection services, string tenant, string clientId, string clientSecret) => 
+        services.AddSingleton(s =>
+        {
+            var authority = string.Format(CultureInfo.InvariantCulture, Instance, tenant);
+            var app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(new Uri(authority))
+                .Build()
+                .AddInMemoryTokenCache();
+            var scopes = new string[] { $"{ApiUrl}.default" };
+            var graphClient = new GraphServiceClient("https://graph.microsoft.com/V1.0/", new DelegateAuthenticationProvider(async (requestMessage) =>
+            {
+                // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
+                var result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+        
+                // Add the access token in the Authorization header of the API request.
+                requestMessage.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            }));
+            var logger = s.GetService<ILogger<TenantGraphClient>>();
+            
+            return new TenantGraphClient(graphClient);
+        });
+    }
+}
---
+++ Services/TenantGraphClient.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Graph;
+
+namespace Services
+{
+    public class TenantGraphClient
+    {
+        private readonly GraphServiceClient _graphClient;
+
+        public TenantGraphClient(GraphServiceClient graphClient)
+        {
+            _graphClient = graphClient;
+        }
+
+        public async Task<Event> RecreateOnlineMeetingEvent(Event remappedEvent)
+        {
+            return await _graphClient
+                .Users[remappedEvent.Organizer.EmailAddress.Address]
+                .Events
+                .Request()
+                .AddAsync(new Event
+                {
+                    AllowNewTimeProposals = remappedEvent.AllowNewTimeProposals,
+                    Attendees = remappedEvent.Attendees,
+                    Body = remappedEvent.Body,
+                    BodyPreview = remappedEvent.BodyPreview,
+                    End = remappedEvent.End,
+                    Importance = remappedEvent.Importance,
+                    IsAllDay = remappedEvent.IsAllDay,
+                    IsCancelled = remappedEvent.IsCancelled,
+                    IsDraft = remappedEvent.IsDraft,
+                    IsOnlineMeeting = remappedEvent.IsOnlineMeeting,
+                    IsOrganizer = remappedEvent.IsOrganizer,
+                    IsReminderOn = remappedEvent.IsReminderOn,
+                    Location = remappedEvent.Location,
+                    Locations = remappedEvent.Locations,
+                    OnlineMeetingProvider = OnlineMeetingProviderType.TeamsForBusiness,
+                    OriginalEndTimeZone = remappedEvent.OriginalEndTimeZone,
+                    OriginalStart = remappedEvent.OriginalStart,
+                    OriginalStartTimeZone = remappedEvent.OriginalStartTimeZone,
+                    Recurrence = remappedEvent.Recurrence,
+                    ReminderMinutesBeforeStart = remappedEvent.ReminderMinutesBeforeStart,
+                    ResponseRequested = remappedEvent.ResponseRequested,
+                    Sensitivity = remappedEvent.Sensitivity,
+                    ShowAs = remappedEvent.ShowAs,
+                    Start = remappedEvent.Start,
+                    Subject = remappedEvent.Subject,
+                    Type = remappedEvent.Type,
+                    Categories = remappedEvent.Categories
+                });
+        }
+
+        public async Task CancelDeprecatedEvent(Event @event)
+        {
+            const string comment = "Canceled by daemon";
+            await _graphClient
+                .Users[@event.Organizer.EmailAddress.Address]
+                .Events[@event.Id]
+                .Cancel(comment)
+                .Request()
+                .PostAsync();
+        }
+        
+        
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            var users = new List<User>();
+            var pagedUsers = await _graphClient.Users
+                .Request()
+                .GetAsync();
+            var pageIterator = PageIterator<User>
+                .CreatePageIterator(
+                    _graphClient,
+                    pagedUsers,
+                    // Callback executed for each item in
+                    // the collection
+                    u =>
+                    {
+                        users.Add(u);
+                        return true;
+                    }
+                );
+            await pageIterator.IterateAsync();
+            return users;
+        }
+
+        public async Task<IEnumerable<Event>> GetMeetingEventsByUser(User user, string dateTimeFrom)
+        {
+            var events = new List<Event>();
+            var queryOptions = new List<QueryOption>()
+            {
+                new("startdatetime", dateTimeFrom),
+                new("enddatetime", DateTime.MaxValue.ToString())
+            };
+            var pagedEvents = await _graphClient.Users[user.Id].Events
+                .Request(queryOptions)
+                .GetAsync();
+            var pageIterator = PageIterator<Event>
+                .CreatePageIterator(
+                    _graphClient,
+                    pagedEvents,
+                    // Callback executed for each item in
+                    // the collection
+                    e =>
+                    {
+                        if ((e.IsOrganizer ?? false) && (e.IsOnlineMeeting ?? false) && (!e.IsCancelled ?? true )) { 
+                            events.Add(e);
+                        }
+                        return true;
+                    }
+                );
+            await pageIterator.IterateAsync();
+            return events;
+        }
+    }
+}

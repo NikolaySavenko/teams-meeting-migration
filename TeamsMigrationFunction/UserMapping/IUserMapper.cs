319804f44a3ea87ea6fda4c8e60f09f0fb553c6f
Nikolay Savenko <savenkons@yandex.ru>
2022-06-20 23:51:32 +0300

    feat: add users mapper entity

---
+++ TeamsMigrationFunction/UserMapping/IUserMapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace TeamsMigrationFunction.UserMapping
+{
+    public interface IUserMapper
+    {
+        Task AddUserMapping(KeyValuePair<string, string> mapping);
+        Task<string> GetUserDestinationUpn(string sourceUpn);
+        Task<Attendee> GetDestinationAttendee(Attendee attendee);
+        Task RecreateUsersMappings(IDictionary<string, string> mapping);
+    }
+}
---
+++ TeamsMigrationFunction/UserMapping/UserMapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using KeyValuePair = System.Collections.Generic.KeyValuePair;
+
+namespace TeamsMigrationFunction.UserMapping
+{
+    public class UserMapper : IUserMapper
+    {
+        [JsonProperty("contacts")]
+        private IDictionary<string, string> UsersMappings { get; set; }
+
+        public Task AddUserMapping(KeyValuePair<string, string> mapping)
+        {
+            UsersMappings.Add(mapping);
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetUserDestinationUpn(string sourceUpn)
+        {
+            var success = UsersMappings.TryGetValue(sourceUpn, out var destinationUpn);
+            return Task.FromResult(success ? destinationUpn : sourceUpn);
+        }
+
+        public async Task<Attendee> GetDestinationAttendee(Attendee attendee)
+        {
+            attendee.EmailAddress.Address = await GetUserDestinationUpn(attendee.EmailAddress.Address);
+            // attendee.Status = new ResponseStatus
+            // {
+            //     Response = ResponseType.NotResponded
+            // };
+            return attendee;
+        }
+
+        public Task RecreateUsersMappings(IDictionary<string, string> mapping)
+        {
+            UsersMappings = mapping;
+            return Task.CompletedTask;
+        }
+        
+        [FunctionName(nameof(UserMapper))]
+        public static Task Run([EntityTrigger] IDurableEntityContext ctx)
+            => ctx.DispatchAsync<UserMapper>();
+
+        public static IDictionary<string, string> ReadMappingFromCsv(string csv)
+        {
+            return new Dictionary<string, string>(
+                csv.Split(Environment.NewLine)
+                    .Select(line => {
+                        var upns = line.Split(",");
+                        return KeyValuePair.Create(upns[0], upns[1]);
+                    })
+            );
+        }
+    }
+}

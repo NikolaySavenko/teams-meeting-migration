ad3cc3242d59b5e2be05b79d9fec7ab810a684ea
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2022-07-10 21:53:09 +0300

    feat: add meeting body correction

--- TeamsMigrationFunction/EventMigration/EventMigrationOrchestration.cs
+++ TeamsMigrationFunction/EventMigration/EventMigrationOrchestration.cs
@@ -38,6 +38,7 @@ namespace TeamsMigrationFunction.EventMigration
             );
             
             var recreatedEvent = await context.CallActivityAsync<Event>(nameof(RecreateOnlineMeetingEvent), sourceEvent);
+            var updatedEvent = await context.CallActivityAsync<Event>(nameof(UpdateMeetingBody), recreatedEvent);
             await context.CallActivityAsync(nameof(CancelDeprecatedEvent), sourceEvent);
             return recreatedEvent;
         }
@@ -48,6 +49,57 @@ namespace TeamsMigrationFunction.EventMigration
             return await _tenantClient.RecreateOnlineMeetingEvent(sourceEvent);
         }
         
+        [FunctionName(nameof(UpdateMeetingBody))]
+        public async Task<Event> UpdateMeetingBody([ActivityTrigger] Event @event)
+        {
+            var optionsUrl = GetOptionsUrlFromOldBody(@event.Body.Content);
+            @event.Body.Content = $@"<html>
+    <head>
+        <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
+    </head>
+    <body>
+        <div>
+            <br>
+            <br>
+            <br>
+            <div style=""width:100%; height:20px"">
+                <span style=""white-space:nowrap; color:#5F5F5F; opacity:.36"">________________________________________________________________________________</span>
+            </div>
+            <div class=""me-email-text"" lang=""en-US"" style=""color:#252424; font-family:'Segoe UI','Helvetica Neue',Helvetica,Arial,sans-serif"">
+                <div style=""margin-top:24px; margin-bottom:20px"">
+                    <span style=""font-size:24px; color:#252424"">Microsoft Teams meeting</span>
+                </div>
+                <div style=""margin-bottom:20px"">
+                    <div style=""margin-top:0px; margin-bottom:0px; font-weight:bold"">
+                        <span style=""font-size:14px; color:#252424"">Join on your computer or mobile app</span>
+                    </div>
+                    <a href=""{@event.OnlineMeeting.JoinUrl}"">Click here to join the meeting</a>
+                </div>
+                <div style=""margin-bottom:24px; margin-top:20px"">
+                    <a href=""https://aka.ms/JoinTeamsMeeting"" class=""me-email-link"" style=""font-size:14px; text-decoration:underline; color:#6264a7; font-family:'Segoe UI','Helvetica Neue',Helvetica,Arial,sans-serif"">Learn More</a> | <a href=""{optionsUrl}"" class=""me-email-link"" style=""font-size:14px; text-decoration:underline; color:#6264a7; font-family:'Segoe UI','Helvetica Neue',Helvetica,Arial,sans-serif"">Meeting options</a>
+                 </div></div><div style=""font-size:14px; margin-bottom:4px; font-family:'Segoe UI','Helvetica Neue',Helvetica,Arial,sans-serif"">
+                </div>
+                <div style=""font-size:12px"">
+                </div>
+            </div>
+            <div style=""width:100%; height:20px"">
+                <span style=""white-space:nowrap; color:#5F5F5F; opacity:.36"">________________________________________________________________________________</span>
+            </div>
+                <div>
+
+                </div>
+            </body>
+                </html>";
+            return await _tenantClient.UpdateEvent(@event);
+        }
+
+        private string GetOptionsUrlFromOldBody(string bodyContent)
+        {
+            var startIndex = bodyContent.IndexOf("https://teams.microsoft.com/meetingOptions/?organizerId");
+            var endIndex = bodyContent.IndexOf("language=");
+            return bodyContent.Substring(startIndex, endIndex - startIndex + 14);
+        }
+
         [FunctionName(nameof(CancelDeprecatedEvent))]
         public async Task CancelDeprecatedEvent([ActivityTrigger] Event @event, ILogger log)
         {

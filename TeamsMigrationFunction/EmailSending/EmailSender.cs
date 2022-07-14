010e9b6192be3fde63a7c41d08a9cd9729832c2b
NikolaySavenko <savenkons@yandex.ru>
2022-07-14 18:27:56 +0300

    fix: improve mail sender

--- TeamsMigrationFunction/EmailSending/EmailSender.cs
+++ TeamsMigrationFunction/EmailSending/EmailSender.cs
@@ -3,45 +3,31 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using TeamsMigrationFunction.MailboxOrchestration;
 
 namespace TeamsMigrationFunction.EmailSending
 {
-    public class EmailSender
+    public static class EmailSender
     {
-        private readonly TenantGraphClient _tenantClient;
-
-        public EmailSender(TenantGraphClient tenantClient)
+        public static DurableHttpRequest BuildEmailRequest(EmailMessage email)
         {
-            _tenantClient = tenantClient;
-        }
-        
-        [FunctionName(nameof(SendMigrationDoneEmail))]
-        public async Task SendMigrationDoneEmail(
-            [ActivityTrigger] User user,
-            ILogger log)
-        {
-            log.LogInformation("[Migration] Successfully sent finished migration email to {UserUserPrincipalName}", user.UserPrincipalName);
-            const string subject = "Your finished migration in Teams";
-            const string body = @"Your Teams meetings has finished.";
-            await _tenantClient.SendEmail(user.UserPrincipalName, subject, body);
-            log.LogInformation("[Migration] Successfully sent finished migration email to {UserUserPrincipalName}", user.UserPrincipalName);
-        }
-        
-        [FunctionName(nameof(SendUpcomingMigrationEmail))]
-        public async Task SendUpcomingMigrationEmail(
-            [ActivityTrigger] User user,
-            ILogger log)
-        {
-            log.LogInformation("[Migration] Trying to send upcoming migration email to {UserUserPrincipalName}", user.UserPrincipalName);
-            const string subject = "Your upcoming migration to Teams";
-            const string body = @"Your account is under migration to Teams.
-App will do this steps:
-1. Recreate every meeting in Teams with updated user names.
-2. Cancel old meetings.
-You will be notified when your migration is done.";
-            await _tenantClient.SendEmail(user.UserPrincipalName, subject, body);
-            log.LogInformation("[Migration] Successfully sent upcoming migration email to {UserUserPrincipalName}", user.UserPrincipalName);
+            var emailEndpoint = Environment.GetEnvironmentVariable("EmailEndpoint");
+            var headers = new Dictionary<string, StringValues>()
+            {
+                {"Content-Type", "application/json"},
+                {"Location", ""}
+            };
+             
+            // var content = "{ \"to\": \"ns@o365hq.com\",\"from\": \"MMS\",\"subject\": \"test\", \"body\": \"some another\"}";
+            var content = JsonConvert.SerializeObject(email);
+            return new DurableHttpRequest(HttpMethod.Post, new Uri(emailEndpoint), headers, content, asynchronousPatternEnabled: false);
         }
     }
+    public record EmailMessage(string from, string to, string subject, string body);
 }
--- TeamsMigrationFunction/MailboxOrchestration/MailboxOrchestration.cs
+++ TeamsMigrationFunction/MailboxOrchestration/MailboxOrchestration.cs
@@ -18,27 +18,61 @@ namespace TeamsMigrationFunction.MailboxOrchestration
         )
         {
             var user = context.GetInput<User>();
-            var retryOptions = new RetryOptions(TimeSpan.FromSeconds(5), 1);
+            
+            await SendUpcomingMigrationEmail(user, context, log);
+            await MigrateMailboxForUser(user, context, log);
+            await SendFinishedMigrationEmail(user, context, log);
+            // Here can be described another orchestrations...
+        }
+        
+        private static async Task MigrateMailboxForUser(User user, IDurableOrchestrationContext context, ILogger log)
+        {
+            if (!context.IsReplaying) log.LogInformation("[Migration] Started mailbox orchestration for user {UserUserPrincipalName}", user.UserPrincipalName);
+            await context.CallSubOrchestratorAsync(nameof(EventsOrchestration.RunEventsOrchestration), user);
+        }
+
+        private static async Task SendUpcomingMigrationEmail(User user, IDurableOrchestrationContext context, ILogger log)
+        {
             try
             {
-                await context.CallActivityWithRetryAsync(nameof(EmailSender.SendUpcomingMigrationEmail), retryOptions, user);
+                log.LogInformation("[Migration] Trying to send upcoming migration email to {UserUserPrincipalName}", user.UserPrincipalName);
+                var email = new EmailMessage(
+                    "Meeting Migration Service",
+                    user.UserPrincipalName,
+                    "Your upcoming migration to Teams",
+                    @"Your account is under migration to Teams.
+App will do this steps:
+1. Recreate every meeting in Teams with updated user names.
+2. Cancel old meetings.
+You will be notified when your migration is done.");
+                var request = EmailSender.BuildEmailRequest(email);
+                await context.CallHttpAsync(request);
+                log.LogInformation("[Migration] Successfully sent upcoming migration email to {UserUserPrincipalName}", user.UserPrincipalName);
             }
             catch (Exception ex)
             {
                 log.LogError("[Migration] Failed to sent upcoming migration email to {userPrincipalName}", user.UserPrincipalName);
             }
-            if (!context.IsReplaying) log.LogInformation("[Migration] Started mailbox orchestration for user {UserUserPrincipalName}", user.UserPrincipalName);
-            await context.CallSubOrchestratorAsync(nameof(EventsOrchestration.RunEventsOrchestration), user);
+        }
+        
+        private static async Task SendFinishedMigrationEmail(User user, IDurableOrchestrationContext context, ILogger log)
+        {
             try
             {
-                await context.CallActivityWithRetryAsync(nameof(EmailSender.SendMigrationDoneEmail), retryOptions, user);
+                log.LogInformation("[Migration] Trying to send finished migration email to {UserUserPrincipalName}", user.UserPrincipalName);
+                var email = new EmailMessage(
+                    "Meeting Migration Service",
+                    user.UserPrincipalName,
+                    "Your finished migration in Teams",
+                    "Your Teams meetings has finished.");
+                var request = EmailSender.BuildEmailRequest(email);
+                await context.CallHttpAsync(request);
+                log.LogInformation("[Migration] Successfully sent finished migration email to {UserUserPrincipalName}", user.UserPrincipalName);
             }
             catch (Exception ex)
             {
-                log.LogError("[Migration] Failed to sent migration done email to {userPrincipalName}", user.UserPrincipalName);
+                log.LogError("[Migration] Failed to sent upcoming migration email to {userPrincipalName}", user.UserPrincipalName);
             }
-            
-            // Here can be described another orchestrations...
         }
     }
 }
